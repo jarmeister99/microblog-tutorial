e9210b917aaac70134aa7e444e79542be84433f3
jarmeister99 <jarmeister99@gmail.com>
2020-01-09 09:34:01 -0800

    finished chapter 4

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 
@@ -7,5 +8,22 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    # one to many - one user, many posts
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     def __repr__(self):
         return f'<User {self.username}>'
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+
+    # always use utc, because it is timezone agnostic
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    # user_id is a field that refers to the primary key of the user model
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
