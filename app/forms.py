b75d344779eb72653eb7f99901bbf87cdb07b5c0
jarmeister99 <jarmeister99@gmail.com>
2020-01-15 09:13:28 -0800

    pagination

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,12 @@ from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Le
 from app.models import User
 
 
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
+
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
