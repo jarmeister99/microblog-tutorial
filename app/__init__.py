69592dfbd42893d7a464b9089a3fe202685d77f1
jarmeister99 <jarmeister99@gmail.com>
2020-01-16 09:36:57 -0800

    finished part 10

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,20 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
+print(f'Mail Server: {app.config["MAIL_SERVER"]}\n'
+      f'Mail Port: {app.config["MAIL_PORT"]}\n'
+      f'Mail Username: {app.config["MAIL_USERNAME"]}')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import os
+import os, secret
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -6,12 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['jarmeister99@gmail.com']
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = (os.environ.get('MAIL_USE_TLS') is not None) or secret.MAIL_USE_TLS
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or secret.MAIL_USERNAME
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or secret.MAIL_PASSWORD
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'jarmeisterbot@gmail.com'
+    ADMINS = ['jarmeisterbot@gmail.com']
     POSTS_PER_PAGE = 15
