87f6e5af92fae48e1eefe5bf764f2221885baf36
jarmeister99 <jarmeister99@gmail.com>
2020-01-16 11:01:21 -0800

    finished part 11

--- app/__init__.py
+++ app/__init__.py
@@ -6,19 +6,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
-print(f'Mail Server: {app.config["MAIL_SERVER"]}\n'
-      f'Mail Port: {app.config["MAIL_PORT"]}\n'
-      f'Mail Username: {app.config["MAIL_USERNAME"]}')
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
